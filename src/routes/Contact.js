975366f307724e08044f5dcefa50aba5ce619991
RUCuwu <afiji.x04@gmail.com>
2024-10-08 09:53:41 -0500

    deploy demo

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import Navbar from "../components/Navbar";
+import Footer from "../components/footer";
+import HeroImg2 from "../components/HeroImg2";
+import Form from "../components/Form";
+
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return <div>
+    <Navbar />
+    <HeroImg2 heading="CONTACT." text="Email Me!" />
+    <Form />
+    <Footer />
+    </div>;
 };
 
 export default Contact;
